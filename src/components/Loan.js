cca3b704b6248d96d4c1541a4e1ba1a8e4b58fc0
MinGong <gongmin00@gmail.com>
2019-11-28 10:48:37 -0800

    add loan component

--- src/components/Loan.js
+++ src/components/Loan.js
@@ -3,42 +3,50 @@ import MainNav from './MainNav'
 import ReactStoreIndicator from 'react-score-indicator'
 import "./style.css"
 
-// const Loan = () => {
-//     let currentLoan =[]
-//     if (currentLoan.length){
-//         return(
-//             <h2>You have unpaid loan</h2>
-//         )
-//     }
-//     return (
-//         <div>
-//             <MainNav />
-//             <div className="loan-text">
-//             <h3>Hi Joe!</h3>
-//             <h4>You have very health credit score</h4>
-//             <h4>Keep It!</h4>
-//             </div>
-//                 <ReactStoreIndicator
-//                     value={70}
-//                     maxValue={100}
-//                 />
-//         </div>
-
-//     )
-
-// };
-class Loan extends React.Component{
-    constructor(props){
+class Loan extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={(
+        this.state = {
+            loanbalance: [100]
+        }
+        this.changeLoanBalance = this.changeLoanBalance.bind(this)
+    }
+    changeLoanBalance(loanValue) {
+        const loanbalance = loanValue
+        this.setState(loanbalance.push(loanValue))
+    }
+    render() {
+        if (!this.state.loanbalance.length) {
+            return (
+                <div>
+                    <MainNav />
+                    <div className="loan-text">
+                        <ReactStoreIndicator
+                            value={70}
+                            maxValue={100} />
+                        <h3>Hi Joe!</h3>
+                        <h4>We realized you have something in your wish list</h4>
+                        <h4>Do you want to request a loan from family bank?</h4>
+                    </div>
+                </div>)
+        }
+        else {
+            return (
+                <div>
+                    <MainNav />
+                    <div className="loan-text">
+                        <ReactStoreIndicator
+                            value={70}
+                            maxValue={100} />
+                        <h3>Hi Joe!</h3>
+                        <h4>You current loan balance is ${this.state.loanbalance}</h4>
+                        <h5>Your minimal payment is $10 and will be due in 20 days</h5>
             
-        )}
-    }
-    render(){
-        return(
-
-        )
-    }
-}
-
-export default Loan;
+                    </div>
+                </div>
+                    )}
+       }
+   }
+   
+   
+export default Loan
---
+++ src/components/LoanSub.js
@@ -0,0 +1,19 @@
+// import React from "react"
+// class LoanSub extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state={
+//             loanbalance:[100]
+//         }
+//     }
+//     changeLoanBalance (){
+//         this.setState:({})
+//     }
+//     render(){
+//         return(
+
+//         )
+//     }
+// }
+
+// export default LoanSub;
